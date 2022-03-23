1ab9c9629eb163744072e2b2004f0910947df0c2
alissatroiano <alissatroianonyc@gmail.com>
2022-03-23 12:09:05 -0400

    Import user loader to repair auth functionality

--- app/auth.py
+++ app/auth.py
@@ -1,20 +1,90 @@
-from flask import Blueprint, render_template, flash, redirect, \
+import os
+from app import login_manager
+from flask import Flask, Blueprint, render_template, flash, redirect, \
                   url_for, session, request
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required, UserMixin
 # from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from app.forms import LoginForm, RegisterForm
+
+from werkzeug.security import generate_password_hash, check_password_hash
+import certifi
+
 from app import mongo
 from app.forms import LoginForm, RegisterForm, ResetPasswordForm
-from app.models.user import User
+from app.config import Config
 
 # Blueprint
 auth = Blueprint("auth", __name__, template_folder='templates')
+
+mongo = PyMongo(tlsCAFile=certifi.where())
+login_manager.login_view = 'login'
+db = mongo.db
+
+# Import User Loader to load user from databases
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+class User(UserMixin):
+    
+    def register(self):
+        print(request.form)
+        
+        # Create the user object
+        user = {
+			"_id": uuid.uuid4().hex,
+			"username": request.form.get("username"),
+   			"email": request.form.get("email"),
+      		"password": request.form.get("password")
+		}
+        
+        # Encrypt the password
+        user['password'] = pbkdf2_sha256.encrypt(user['password'])
+        
+        return jsonify(user), 200
+    
+    def __init__(self, username):
+        self.username = username
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.username
+
+    @staticmethod
+    def check_password(password_hash, password):
+        return check_password_hash(password_hash, password)
+
+    @staticmethod
+    def check_password(password_hash, password):
+        return check_password_hash(password_hash, password)
+
+    @login_manager.user_loader
+    def load_user(username):
+        u = mongo.db.Users.find_one({"Name": username})
+        if not u:
+            return None
+        return User(username=u['username'])
 
 
 # A route to render the register page and add users to database
 @auth.route("/register", methods=["GET", "POST"])
 def register():
+    form = RegisterForm()
     if request.method == "POST":
         # check if username already exists in db
         current_user = mongo.db.users.find_one(
